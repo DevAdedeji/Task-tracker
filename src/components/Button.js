dc91fb27602b41b2e72776a7c0bd1fb1e243f19d
DevAdedeji <adedejitewogbade2@gmail.com>
2022-05-18 14:17:23 +0100

    Made Header

---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types'
+
+import React from 'react'
+
+const Button = ({ color, text, onClick }) => {
+    return (
+        <button onClick={onClick} className='btn' style={{ backgroundColor: color }}>{text}</button>
+    )
+}
+
+Button.defaultProps = {
+    text: "Add",
+    color: "steelblue"
+}
+
+Button.propTypes = {
+    text: PropTypes.string.isRequired,
+    color: PropTypes.string,
+    onClick: PropTypes.func,
+}
+
+
+export default Button
