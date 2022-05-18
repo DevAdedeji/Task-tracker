136847cca65209a674f3c6c450e59d9778d7e480
DevAdedeji <adedejitewogbade2@gmail.com>
2022-05-18 15:06:13 +0100

    Done with deleteTask, toggleReminder

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,33 +1,13 @@
 import React from 'react'
+import Task from './Task'
 
-const tasks = [
-    {
-        id: 1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting st school',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    }
-]
-
-const Tasks = () => {
+const Tasks = ({ tasks, OnDelete, onToggle }) => {
     return (
         <>
             {
-                tasks.map((task) => {
-                    <h3 key={task.id}>{task.text}</h3>
-                })
+                tasks.map((task) => (
+                    <Task key={task.id} task={task} onDelete={OnDelete} OnToggle={onToggle}></Task>
+                ))
 
             }
         </>
