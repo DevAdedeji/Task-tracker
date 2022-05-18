394aa7ef5637132c6851d95864edb48228eda2a1
DevAdedeji <adedejitewogbade2@gmail.com>
2022-05-18 15:34:01 +0100

    Done with AddTask Functionality

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from './components/AddTask';
 
 
 function App() {
@@ -24,6 +25,13 @@ function App() {
       reminder: false,
     }
   ])
+
+  // Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 100000) + 1
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
+  }
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) =>
@@ -35,9 +43,11 @@ function App() {
     setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
   }
 
+
   return (
     <div className="container">
       <Header />
+      <AddTask onAdd={addTask} />
       {
         tasks.length > 0 ? <Tasks tasks={tasks} OnDelete={deleteTask} onToggle={toggleReminder} /> : <h1>No Tasks</h1>
       }
